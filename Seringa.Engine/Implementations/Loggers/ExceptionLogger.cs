14b17ce19e7d1a986b521d2e9b1aac7052b88a79
Lucas A. de Araújo <lucasdearaujo@icloud.com>
2013-05-03 23:43:21 -0300

    Exception logger

---
+++ Seringa.Engine/Implementations/Loggers/ExceptionLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Seringa.Engine.Implementations.Loggers
+{
+    class ExceptionLogger
+    {
+        /// <summary>
+        /// Method to log a exception into a textfile.
+        /// </summary>
+        /// <typeparam name="T">Exception Type</typeparam>
+        /// <param name="fileLocation">File that will be writen</param>
+        /// <param name="exception">Exception to be logged</param>
+        public static void Write<T>(String fileLocation, T exception)
+            where T : Exception
+        {
+            try
+            {
+                if (!File.Exists(fileLocation))
+                    File.Create(fileLocation);
+
+                using (FileStream fileStream = File.Open(fileLocation, FileMode.Open, FileAccess.Write))
+                {
+                   var message = Encoding.UTF8.GetBytes(
+                                    "[" + exception.Source + "] " + exception.Message  + "\n\n" 
+                                     + exception.StackTrace + "\n\n\n");
+
+                   fileStream.Write(message, 0, message.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Better you decide....
+            }
+        }
+    }
+}
