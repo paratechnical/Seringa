0614726a9034a5573526c6243e3d7886852a8aaf
Lucas A. de Araújo <lucasdearaujo@icloud.com>
2013-05-04 00:17:52 -0300

    Most clear exception message.

--- Seringa.Engine/Implementations/Loggers/ExceptionLogger.cs
+++ Seringa.Engine/Implementations/Loggers/ExceptionLogger.cs
@@ -4,34 +4,36 @@ using System.Text;
 
 namespace Seringa.Engine.Implementations.Loggers
 {
-    class ExceptionLogger
+    public class ExceptionLogger
     {
         /// <summary>
         /// Method to log a exception into a textfile.
         /// </summary>
-        /// <typeparam name="T">Exception Type</typeparam>
         /// <param name="fileLocation">File that will be writen</param>
         /// <param name="exception">Exception to be logged</param>
-        public static void Write<T>(String fileLocation, T exception)
-            where T : Exception
+        public static void Write(String fileLocation, Exception exception)
         {
             try
             {
                 // If file do not exis, create it
                 if (File.Exists(fileLocation) == false)
-                    File.Create(fileLocation);
+                    File.Create(fileLocation).Close();
 
                 // Open the file to write the message
                 using (FileStream fileStream = 
-                    File.Open(fileLocation, FileMode.Open, FileAccess.Write))
+                    File.Open(fileLocation, FileMode.Append, FileAccess.Write))
                 {
                     // Generate the message
                     var message = Encoding.UTF8.GetBytes(
-                                    "[" + exception.Source + "] " + exception.Message  + "\n\n" 
-                                     + exception.StackTrace + "\n\n\n");
+                                    "[" + DateTime.Now + " : " + exception.Source + "] " 
+                                     + exception.GetType() + " "
+                                     + exception.Message 
+                                     + exception.StackTrace
+                                     + Environment.NewLine);
 
                     // Write the message down.
                     fileStream.Write(message, 0, message.Length);
+                    fileStream.Close();
                 }
             }
             catch (Exception ex)
